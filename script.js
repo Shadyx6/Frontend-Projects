28bc2ecb51899a11394f6f58f679f2409dacf4f1
Shadyx6 <shadkhanxox@gmail.com>
2024-02-29 14:13:25 +0530

    Updated index.html and script.js with new features

--- script.js
+++ script.js
@@ -2,12 +2,41 @@ let newnotebtn = document.querySelector('.new')
 let overlay = document.querySelector('.overlay')
 let qry = document.querySelector('.field')
 let init = document.querySelector('.empty')
+var title = document.querySelector('.input-title')
+let details = document.querySelector('.details')
+let save = document.querySelector('.save')
 
-function newnote(){
-newnotebtn.addEventListener('click', function(){
-    overlay.style.display = 'block'
-    qry.style.display = 'block'
-    init.style.display = 'none'
-})
+if (localStorage.getItem('tasks') === null) {
+    localStorage.setItem('tasks', JSON.stringify([]))
+}
+
+
+function newnote() {
+    newnotebtn.addEventListener('click', function () {
+        overlay.style.display = 'block'
+        qry.style.display = 'block'
+        init.style.display = 'none'
+    })
 }
 newnote()
+save.addEventListener('click', function () {
+    const tit = title.value
+    const dets = details.value
+
+    const dataofinputs = {
+        title: tit,
+        details: dets
+    }
+    // Take previous tasks out:
+    let previoustasks = localStorage.getItem('tasks')
+    // Parse em all:
+    let preparsedTasks = JSON.parse(previoustasks) || [];
+    console.log(preparsedTasks)
+    // Add more tasks in them through fetchdata function:
+    preparsedTasks.push(dataofinputs)
+    // Stringify em again:
+    let strigifiedalltasks = JSON.stringify(preparsedTasks)
+    // ADD THEM BACK TO LOCAL STORAGE 
+    localStorage.setItem('tasks', strigifiedalltasks)
+})
+
